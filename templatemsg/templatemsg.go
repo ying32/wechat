7544f0ec89648f720d2c4c2bdef5a0b4aa4dbe0a
ying32 <zying32@163.com>
2017-06-16 10:19:53 +0800

    增加模板消息数据类型

--- templatemsg/templatemsg.go
+++ templatemsg/templatemsg.go
@@ -39,6 +39,33 @@ type TTemplateDataVal struct {
 	Color string `json:"color"`
 }
 
+type TTemplateData struct {
+	datas map[string]TTemplateDataVal
+}
+
+// NewTempldateData 新建一个模板数据
+func NewTempldateData() *TTemplateData {
+	td := new(TTemplateData)
+	td.datas = make(map[string]TTemplateDataVal, 0)
+	return td
+}
+
+// Add 添加模板数据
+func (t *TTemplateData) Add(key, value, color string) {
+	data := TTemplateDataVal{value, color}
+	t.datas[key] = data
+}
+
+// Del 删除模板数据
+func (t *TTemplateData) Del(key string) {
+	delete(t.datas, key)
+}
+
+// Data 返回数据
+func (t *TTemplateData) Data() interface{} {
+	return t.datas
+}
+
 // NewTemplateMsg 构造
 func NewTemplateMsg(context *context.Context) *TTemplateMsg {
 	tmsg := new(TTemplateMsg)
@@ -47,7 +74,7 @@ func NewTemplateMsg(context *context.Context) *TTemplateMsg {
 }
 
 // PushTo 推送业务消息
-func (t *TTemplateMsg) PushTo(openId, templateId, url, topColor string, data interface{}) error {
+func (t *TTemplateMsg) PushTo(openId, templateId, url, topColor string, data *TTemplateData) error {
 	accessKey, err := t.GetAccessToken()
 	if err != nil {
 		return err
@@ -57,7 +84,7 @@ func (t *TTemplateMsg) PushTo(openId, templateId, url, topColor string, data int
 	msgBody.TemplateId = templateId
 	msgBody.TopColor = topColor
 	msgBody.URL = url
-	msgBody.Data = data
+	msgBody.Data = data.Data()
 	response, err := util.PostJSON(fmt.Sprintf(templateMsg, accessKey), msgBody)
 	var result TTemplateMsgError
 	err = json.Unmarshal(response, &result)
@@ -71,6 +98,6 @@ func (t *TTemplateMsg) PushTo(openId, templateId, url, topColor string, data int
 }
 
 // PushTo2 推送业务消息， 使用默认的模板Id
-func (t *TTemplateMsg) PushTo2(openId, url, topColor string, data interface{}) error {
+func (t *TTemplateMsg) PushTo2(openId, url, topColor string, data *TTemplateData) error {
 	return t.PushTo(openId, t.TemplateMsgId, url, topColor, data)
 }
