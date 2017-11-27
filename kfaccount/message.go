b309d2f69066e04f248ac0db4fb12251668dcc20
ying32 <ying_33@live.cn>
2017-11-27 09:45:10 +0800

    增加了客服接口customsend回复文本及语音消息接口

--- kfaccount/message.go
+++ kfaccount/message.go
@@ -1,15 +1,32 @@
 package kfaccount
 
-type TReply struct {
+type TReplyCommon struct {
 	ToUser  string `json:"touser"`
 	MsgType string `json:"msgtype"`
 }
 
+// TImageReply 图像消息
 type TImageReply struct {
-	TReply
+	TReplyCommon
 	Image struct {
 		Media_id string `json:"media_id"`
 	} `json:"image"`
+}
+
+// TTextReply 文字消息
+type TTextReply struct {
+	TReplyCommon
+	Text struct {
+		Content string `json:"content"`
+	} `json:"text"`
+}
+
+// TVoiceReply 语音消息
+type TVoiceReply struct {
+	TReplyCommon
+	Voice struct {
+		Media_id string `json:"media_id"`
+	} `json:"voice"`
 }
 
 func NewImageReply(touser, mediaid string) *TImageReply {
@@ -17,5 +34,21 @@ func NewImageReply(touser, mediaid string) *TImageReply {
 	im.MsgType = "image"
 	im.ToUser = touser
 	im.Image.Media_id = mediaid
+	return im
+}
+
+func NewTextReply(touser, content string) *TTextReply {
+	im := new(TTextReply)
+	im.MsgType = "text"
+	im.ToUser = touser
+	im.Text.Content = content
+	return im
+}
+
+func NewImageReply(touser, mediaid string) *TVoiceReply {
+	im := new(TVoiceReply)
+	im.MsgType = "voice"
+	im.ToUser = touser
+	im.Voice.Media_id = mediaid
 	return im
 }
